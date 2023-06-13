e525658bb194803e07576d659a3a5c57db059aec
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2023-06-13 16:29:31 +0300

    added user data from local storage

--- src/components/CustomDrawer.jsx
+++ src/components/CustomDrawer.jsx
@@ -1,17 +1,34 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
+import {useEffect, useState} from 'react';
 
+import {useSelector} from 'react-redux';
 import {Icon} from '@rneui/themed';
 
-const CustomDrawer = props => {
-  console.log(props);
+const CustomDrawer = () => {
+  const username = useSelector(state => state.userData.firstName);
+  const meterNumber = useSelector(state => state.userData.meterNumber);
+  const phoneNumber = useSelector(state => state.userData.phoneNumber);
+  const region = useSelector(state => state.userData.region);
+  const district = useSelector(state => state.userData.district);
+
+  const [userData, setUserData] = useState({});
+
+  const handleLogout = () => {
+    console.log('logout');
+  };
+
   return (
     <View className="flex-1">
       <View className="bg-[#5a8fbb] px-4 py-4">
         <View className="bg-[#5085b1] rounded-full w-20 h-20 items-center justify-center">
-          <Text className="text-white font-bold text-4xl">D</Text>
+          <Text className="text-white font-bold text-4xl">
+            {username.substring(0, 1)}
+          </Text>
         </View>
-        <Text className="text=[#fcffff] font-bold text-2xl pt-4">Drack007</Text>
-        <Text className=" pt-1 text-lg text-gray-300">+255743661350</Text>
+        <Text className="text=[#fcffff] font-bold text-2xl pt-4">
+          {username}
+        </Text>
+        <Text className=" pt-1 text-lg text-gray-300">{phoneNumber}</Text>
       </View>
       <View className="px-3 pt-4 space-y-4">
         <View className="flex-row space-x-4 items-center ">
@@ -25,7 +42,7 @@ const CustomDrawer = props => {
             <Text className="text-[#636363] text-lg font-semibold">
               Meter Number
             </Text>
-            <Text className="text-[#8b9197] text-lg">1234567890</Text>
+            <Text className="text-[#8b9197] text-lg">{meterNumber}</Text>
           </View>
         </View>
 
@@ -41,14 +58,14 @@ const CustomDrawer = props => {
               Physical Address
             </Text>
             <Text className="text-[#8b9197] text-lg">
-              Dar-es-salaam, Kinondoni
+              {`${region},${district}`}
             </Text>
           </View>
         </View>
       </View>
       <View className="p-4 absolute bottom-0 flex-row items-center space-x-4">
-        <Icon name="logout" size={32} color="#8b9197" />
-        <TouchableOpacity>
+        <Icon name="logout" size={32} color="#3da2f5" />
+        <TouchableOpacity onPress={handleLogout}>
           <Text className="text-lg text-black font-bold">Sign out</Text>
         </TouchableOpacity>
       </View>
@@ -57,3 +74,44 @@ const CustomDrawer = props => {
 };
 
 export default CustomDrawer;
+
+// import { useEffect, useState } from 'react';
+// import {View, Text, TouchableOpacity} from 'react-native';
+
+// import {Icon} from '@rneui/themed';
+// import {useDispatch, useSelector} from 'react-redux';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import * as SecureStore from 'expo-secure-store';
+
+// import {setLogin} from '../redux/slice/login';
+// import { deleteToken } from '../redux/slice/tokenSlice';
+// import { deleteUserData } from '../redux/slice/userDataSlice';
+
+// const CustomDrawer = props => {
+//   const dispatch = useDispatch();
+//   const [user, setUser] = useState({})
+
+//   const handleLogout = async () => {
+//     await SecureStore.deleteItemAsync('auth-token')
+//     console.log('token deleted');
+//     await AsyncStorage.removeItem('userData')
+//     console.log('deleted user data');
+//     dispatch(setLogin(false))
+//   };
+
+//   const getUserData = async () => {
+//     await AsyncStorage.getItem('userData')
+//     .then(val => {
+//       const user = JSON.parse(val)
+//       setUser(user)
+//       console.log(user);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     })
+//   }
+//   useEffect(() => {
+//     getUserData()
+//   }, [])
+
+// export default CustomDrawer;
