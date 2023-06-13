d85402ece26769b1b10771d0b81d965c94383f5a
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2023-06-13 16:30:33 +0300

    readded the customdrawer

--- src/Navigator/DrawerNavigator.jsx
+++ src/Navigator/DrawerNavigator.jsx
@@ -1,16 +1,37 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import HomeScreen from '../screens/HomeScreen';
 import CustomDrawer from '../components/CustomDrawer';
+import {Badge, Icon, withBadge} from '@rneui/themed';
+import {useNavigation} from '@react-navigation/native';
+
+const BadgedIcon = withBadge(15)(Icon);
 
 const DrawerNavigator = () => {
   const Drawer = createDrawerNavigator();
+  const navigation = useNavigation();
 
   return (
     <View className="flex-1">
-      <Drawer.Navigator drawerContent={props => <CustomDrawer props={props} />}>
+      <Drawer.Navigator
+        drawerContent={props => <CustomDrawer props={props} />}
+        screenOptions={{
+          headerRight: () => (
+            <TouchableOpacity
+              className="pr-7"
+              onPress={() => navigation.navigate('Notification')}>
+              <BadgedIcon
+                type="ionicon"
+                size={32}
+                name="notifications-outline"
+              />
+            </TouchableOpacity>
+          ),
+          title: 'Smart Water Meter ',
+        }}>
+        {/* drawerContent={props => <CustomDrawer props={props} />} */}
         <Drawer.Screen name="Home" component={HomeScreen} />
       </Drawer.Navigator>
     </View>
