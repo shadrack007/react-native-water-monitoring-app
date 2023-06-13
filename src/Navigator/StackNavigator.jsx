47c1be313558e40c505c8264dc410f51aecc2527
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2023-06-13 16:11:16 +0300

    modified promise handling in getuserdata

--- src/Navigator/StackNavigator.jsx
+++ src/Navigator/StackNavigator.jsx
@@ -1,31 +1,79 @@
-import {useState} from 'react';
-import {View, Text} from 'react-native';
+import {useEffect, useState} from 'react';
+import {View} from 'react-native';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
+import {useDispatch, useSelector} from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import MeterNumberScreen from '../screens/MeterNumberScreen';
 import DrawerNavigator from './DrawerNavigator';
-import LoginScreen from '../screens/LoginScreen';
-import SignupScreen from '../screens/SignupScreen';
+import NotificationScreen from '../screens/NotificationScreen';
+import {
+  setMeterNumber,
+  setFirstName,
+  setPhoneNumber,
+  setRegion,
+  setDistrict,
+} from '../redux/slice/userDataSlice';
+import {setLogin} from '../redux/slice/login';
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
-  const [isSignedIn, setIsSignedIn] = useState(true);
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(state => state.login.value);
+
+  const getUserData = async () => {
+    await AsyncStorage.getItem('userData')
+      .then(data => {
+        if (data) {
+          const userDataObject = JSON.parse(data);
+          dispatch(setFirstName(userDataObject.customer.first_name));
+          dispatch(setMeterNumber(userDataObject.meter_number));
+          dispatch(setPhoneNumber(userDataObject.customer.meter_number));
+          dispatch(setRegion(userDataObject.customer.region));
+          dispatch(setDistrict(userDataObject.customer.district));
+          dispatch(setLogin(true));
+        } else {
+          console.log('no data found');
+          dispatch(setLogin(false));
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .finally(() => {
+        SplashScreen.hide();
+      });
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <View className="flex-1">
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        {isSignedIn ? (
+      <Stack.Navigator>
+        {isLoggedIn ? (
           <>
-            <Stack.Screen name="Main" component={DrawerNavigator} />
+            <Stack.Screen
+              name="Main"
+              component={DrawerNavigator}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen name="Notification" component={NotificationScreen} />
           </>
         ) : (
           <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Signup" component={SignupScreen} />
+            <Stack.Screen
+              name="MeterNumber"
+              component={MeterNumberScreen}
+              options={{headerShown: false}}
+            />
           </>
         )}
       </Stack.Navigator>
     </View>
   );
 };
-
 export default StackNavigator;
