ad10ec377c90dc945525b6ca7022cac54769ad33
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2023-06-23 08:07:59 +0300

    main replaced stack navigator

--- App.jsx
+++ App.jsx
@@ -1,19 +1,28 @@
 import 'react-native-gesture-handler';
+import {useEffect} from 'react';
 import {View, Text, SafeAreaView, StatusBar} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import {Provider} from 'react-redux';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import StackNavigator from './src/Navigator/StackNavigator';
 import {store} from './src/redux/store';
+import Main from './src/Main';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
   return (
     <SafeAreaView className="flex-1">
-      <StatusBar backgroundColor="#5a8fbb" />
-      <NavigationContainer>
-        <StackNavigator />
-      </NavigationContainer>
+      <Provider store={store}>
+        <StatusBar backgroundColor="#5a8fbb" />
+        <GestureHandlerRootView className="flex-1">
+          <Main />
+        </GestureHandlerRootView>
+      </Provider>
     </SafeAreaView>
   );
 };
