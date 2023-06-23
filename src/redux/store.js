dfca84acc5e574e38171c9cc4fcb24eecbe58d33
shadrack007 <shadrack.h.mwakibinga@gmail.com>
2023-06-23 08:08:55 +0300

    removied unused reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit';
 
 import loginReducer from './slice/login';
+import userDataReducer from './slice/userDataSlice';
+import meterNumberReducer from './slice/meterNumberSlice';
 
 export const store = configureStore({
   reducer: {
+    meterNumber: meterNumberReducer,
     login: loginReducer,
+    userData: userDataReducer,
   },
 });
